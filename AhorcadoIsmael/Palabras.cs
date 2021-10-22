b8adae5dabc090d773cc408acdc154d5d4c774de
IsmaDevBalmis <75784444+IsmaDevBalmis@users.noreply.github.com>
2021-10-22 12:02:58 +0200

    por si aca v2

---
+++ AhorcadoIsmael/Palabras.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AhorcadoIsmael
+{
+    class Palabras
+    {
+
+        static List<String> palabrasFaciles = new List<string>(){ 
+        
+            "caca",
+            "perro",
+            "mesa"
+
+        };
+
+
+        static List<String> palabrasMedias = new List<string>(){
+
+            "mochila",
+            "programacion",
+            "ordenador"
+
+        };
+
+        static List<String> palabrasDificiles = new List<string>(){
+
+            "esternocleidomastoideo",
+            "estrabismo",
+            "maricon"
+
+
+        };
+
+        static List<String> palabrasUsuario = new List<string>(){
+
+            
+
+
+        };
+
+
+    }
+}
